f58439ac28befebe45f1463095658f404b818be1
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2019-10-15 00:26:03 -0300

    webpack config

---
+++ curso-es6/src/main.js
@@ -0,0 +1,13 @@
+//import { soma as somaFunction, sub } from './functions'; //renomeando a funcao para outro nome
+//import soma from './soma'; //quando usando default em function soma
+//import soma, { sub } from './functions'; //quando usando default em function soma e sub sem default
+//import * as functions from './functions';
+
+//console.log(functions);
+//console.log(functions.soma(1, 3));
+//console.log(functions.sub(4, 2));
+
+//console.log(soma(1, 3));
+//console.log(sub(4, 2));
+
+alert('Testando!')
