d1df42b8c732d50fb4f37587a522698d03f2ce87
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2019-10-15 12:19:06 -0300

    exercicios modulo 3

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -20,6 +20,7 @@
 Usuario.info();
 console.log(IdadeUsuario);*/
 
+//---------------------------------------------------------------
 /*const minhaPromise = () => new Promise((resolve, reject) => {
   setTimeout(() => { resolve('OK') }, 2000);
 });*/
@@ -38,7 +39,7 @@ executaPromise();*/
 }
 executaPromise();*/
 
-import axios from 'axios';
+/*import axios from 'axios';
 
 class Api {
   static async getUserInfo(username) {
@@ -51,4 +52,63 @@ class Api {
   }
 }
 Api.getUserInfo('ebraimcarvalho');
-Api.getUserInfo('ebraimcarvalho123123123');
+Api.getUserInfo('ebraimcarvalho123123123');*/
+
+//------------------Exercicios Modulo 3------------------------------------
+// Função delay aciona o .then após 1s
+// A
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+async function umPorSegundo() {
+  await delay();
+  console.log("1s");
+
+  await delay();
+  console.log("2s");
+
+  await delay();
+  console.log("3s");
+}
+umPorSegundo();
+
+// B
+
+import axios from 'axios';
+
+async function getUserFromGithub(user) {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${user}`);
+    console.log(response.data);
+  } catch (err) {
+    console.log('Usuário não existe');
+  }
+}
+getUserFromGithub('diego3g');
+getUserFromGithub('diego3g124123');
+
+// C
+
+class Github {
+  static async getRepositories(username) {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${username}/repos`);
+      console.log(response.data);
+    } catch (err) {
+      console.log('Repositório não existe');
+    }
+  }
+ }
+ Github.getRepositories('rocketseat');
+ Github.getRepositories('ebraimcarvalho');
+
+ // D
+
+ const buscaUsuario = async usuario => {
+   try {
+    const response = await axios.get(`https://api.github.com/users/${usuario}`);
+    console.log(response.data);
+   } catch (err) {
+    console.log('Usuário não existe');
+   }
+ }
+ buscaUsuario('diego3g');
