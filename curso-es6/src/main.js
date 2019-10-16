b80447fa0494ab1d7ea34dde50789956197305bb
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2019-10-16 00:38:04 -0300

    comentando codigo main.js do modulo 4

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -127,7 +127,7 @@ import api from './api';
 
 class App {
   constructor() {
-    this.repositories = [];
+    this.repositories = []; // cria um array vazio chamado repositories
 
     this.formEl = document.getElementById('repo-form');
     this.inputEl = document.querySelector('input[name=repository]');
@@ -137,83 +137,82 @@ class App {
   }
 
   registerHandlers() {
-    this.formEl.onsubmit = event => this.addRepository(event);
+    this.formEl.onsubmit = event => this.addRepository(event); // ao submeter o formulario, chama a função addRepository(event) passando o evento
   }
 
-  setLoading(loading = true) {
-    if (loading === true) {
+  setLoading(loading = true) { // recebe como padrão loading === true
+    if (loading === true) { // se loading é true então cria um span contendo o texto 'Carregando...' com id="loading"
       let loadingEl = document.createElement('span');
       loadingEl.appendChild(document.createTextNode('Carregando...'));
       loadingEl.setAttribute('id', 'loading');
 
-      this.formEl.appendChild(loadingEl)
-    } else {
+      this.formEl.appendChild(loadingEl) // formulário recebe o elemento span Carregando...
+    } else { // se loading for false, remove o span Carregando...
       document.getElementById('loading').remove();
     }
   };
 
-  async addRepository(event) {
+  async addRepository(event) { //função assíncrona que recebe o event e evita que ao submeter o form, a página recarregue
     event.preventDefault();
 
-    const repoInput = this.inputEl.value;
+    const repoInput = this.inputEl.value; // recebe o valor do campo input na const repoInput
 
-    if (repoInput.length === 0)
-      return;
+    if (repoInput.length === 0) // se o input estiver vazio, retorna um alerta solicitando que seja preenchido para buscar
+      return alert('Preencha o campo para realizar a busca!');;
 
-    this.setLoading();
+    this.setLoading(); // se não estiver vazio, chama a função setLoading()
 
-    try {
+    try { // em caso de sucesso na requisição, busca o repositorio do usuario na base da api.github e aloca em const response
       const response = await api.get(`/repos/${repoInput}`);
 
-      const { name, description, html_url, owner: { avatar_url } } = response.data
+      const { name, description, html_url, owner: { avatar_url } } = response.data; //declara variaveis name, description, html_url e avatar_url que será extraído de response.data 
 
-      this.repositories.push({
+      this.repositories.push({ //incluirá em repositories o name, description, avatar_url e html_url
         name,
         description,
         avatar_url,
         html_url,
       });
 
-      this.inputEl.value = '';
+      this.inputEl.value = ''; //limpa o campo do input
 
-      this.render();
-    } catch (err) {
+      this.render(); // chama a função para renderizar
+    } catch (err) { // em caso de requisição com falha, cria um alerta na tela: "O repositório não existe!"
       alert('O repositório não existe!');
-      this.inputEl.value = '';
+      this.inputEl.value = ''; //limpa o campo input
     }
 
-    this.setLoading(false);
+    this.setLoading(false); //chama a função setLoading(false) passando loading como false, onde será removido o texto Carregando...
   }
 
   render() {
-    this.listEl.innerHTML = '';
+    this.listEl.innerHTML = ''; //a lista começa em branco
 
-    this.repositories.forEach(repo => {
-      let imgEl = document.createElement('img');
+    this.repositories.forEach(repo => { //chama a const repositories passando por cada elemento dela, atribuindo repo a cada uma onde... 
+      let imgEl = document.createElement('img'); //cria uma imagem com src do avatar_url do repositorio
       imgEl.setAttribute('src', repo.avatar_url);
 
-      let titleEl = document.createElement('strong');
+      let titleEl = document.createElement('strong'); //cria um strong com o nome do repositorio
       titleEl.appendChild(document.createTextNode(repo.name));
 
-      let descriptionEl = document.createElement('p');
+      let descriptionEl = document.createElement('p'); //cria um paragrafo com a descricao do repositorio
       descriptionEl.appendChild(document.createTextNode(repo.description));
 
-      let linkEl = document.createElement('a');
+      let linkEl = document.createElement('a'); //cria um link com target _blank e href com o html_url do repositorio, alem de criar um texto Acessar
       linkEl.setAttribute('target', '_blank');
       linkEl.setAttribute('href', repo.html_url);
       linkEl.appendChild(document.createTextNode('Acessar'));
 
-      let listItemEl = document.createElement('li');
+      let listItemEl = document.createElement('li'); // cria uma li que recebe a imagem, o nome, a descricao e o link do repositorio
       listItemEl.appendChild(imgEl);
       listItemEl.appendChild(titleEl);
       listItemEl.appendChild(descriptionEl);
       listItemEl.appendChild(linkEl);
 
-      this.listEl.appendChild(listItemEl);
+      this.listEl.appendChild(listItemEl); //a lista ul recebe a li em questão
     })
   }
 }
 
 new App();
-console.log('teste');
 console.log('teste');
