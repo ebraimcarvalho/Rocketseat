3cdb21906710c9e89ffbb00f2c2174750924a89b
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2019-10-18 16:58:57 -0300

    node-api concluído cors

--- node-api/server.js
+++ node-api/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Iniciando o DB
 mongoose.connect('mongodb://192.168.99.100:27017/nodeapi', { 
   useNewUrlParser: true,
-  useUnifiedTopology: true, 
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 requireDir('./src/models');
 
